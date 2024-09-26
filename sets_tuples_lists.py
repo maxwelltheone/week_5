e679936b9fba508df375f4e7e8d8ffa2cf97be58
Luevanos <pluevanos@cps.k12.il.us>
2024-09-26 08:52:10 -0500

    Cars update

--- sets_tuples_lists.py
+++ sets_tuples_lists.py
@@ -3,22 +3,53 @@
 #   Set   = {} unordered and immutable, but Add/Remove OK. NO duplicates
 #   Tuple = () ordered and unchangeable. Duplicates OK. FASTER
 
-#fruits = ["Apple", "Orange", "Banana", "Coconut", "Kiwi", "Pear", "Strawberry", "Pineapple"]
-#print(dir(fruits)) #Prints all methods
-#print(help(fruits)) #Get help
-#print(len(fruits)) #
-#print("Apple" in fruits) #Boolean method (True or False)
+# fruits = ["Apple", "Orange", "Banana", "Coconut", "Kiwi", "Pear", "Strawberry", "Pineapple"]
+# print(dir(fruits)) #Prints all methods
+# print(help(fruits)) #Get help
+# print(len(fruits)) #
+# print("Apple" in fruits) #Boolean method (True or False)
 
-#fruits[0] = "Mango" #Reassign values
-#fruits.append("Mango") # Move variable to end
-#fruits.remove("apple") # Removes value
-#fruits.insert(0, "Mango") # Sets variables at certain value
-#fruits.sort() # Sorts by alphabet
-#fruits.reverse() # Reverses order of list
-#fruits.clear() # Kills list
+# fruits[0] = "Mango" #Reassign values
+# fruits.append("Mango") # Move variable to end
+# fruits.remove("apple") # Removes value
+# fruits.insert(0, "Mango") # Sets variables at certain value
+# fruits.sort() # Sorts by alphabet
+# fruits.reverse() # Reverses order of list
+# fruits.clear() # Kills list
 
-#print(fruits)
+# print(fruits)
 
 # print(fruits[0])
 # for fruit in fruits:
    # print(fruit)
+
+cars = ["BMW", "Maserati", "Audi", "Mercedes","Ferrari"]
+print(f"these are a list of {cars}")
+print(f"The first being from {cars[0]}")
+
+# changing value of the list
+cars[0] = "Lightning Mcqueen"
+print(f"The first car is {cars[0]}")
+
+print(f"the last car is {cars[-1]}")
+cars[-1] = "Lamborghini"
+print(f"the last car is {cars[-1]}")
+
+# adding a new value
+cars.append("Bugatti")
+print(cars)
+cars.remove("Maserati")
+print(cars)
+
+# Looping through the list (AKA iterating)
+for car in cars:
+   # print(len(car))
+   # print(car)
+    carRequest = input("Add a new car please: ")
+    cars.append(carRequest)
+    print(cars)
+    print(len(cars))
+    print(cars.upper())
+    print(cars)
+    if len(cars) > 10:
+        break
